6962ae85719e335ac034599e332d84fb5b50b27b
Sutanu Sain <sutanu.sain@gmail.com>
2021-05-13 22:37:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [greetings, setGreetings] = useState(null);
+  const onclicking = () => {
+     setGreetings("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+  }
   return (
     <div id="main">
+    <button id="click" type ="button" onClick = {onclicking}>Click Me</button>
+    <para id="para">{greetings}</para>
       // Do not alter the main div
     </div>
   );
